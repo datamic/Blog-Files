b6883329fe0ec6a1fae36a11ed8a51a3e1cab5fa
datamic <datamic@outlook.com>
2017-09-04 21:39:00 -0500

    Published with blog

--- Content/HDI Spark and ADF/Pyspark Scripts/pysparkArgs.py
+++ Content/HDI Spark and ADF/Pyspark Scripts/pysparkArgs.py
@@ -1,3 +1,4 @@
+
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder \
--- Content/HDI Spark and ADF/Pyspark Scripts/pysparkSimple.py
+++ Content/HDI Spark and ADF/Pyspark Scripts/pysparkSimple.py
@@ -1,5 +1,5 @@
-from pyspark.sql import SparkSession
 
+from pyspark.sql import SparkSession
 
 spark = SparkSession.builder \
      .appName("SparkonADF - Simple")\
